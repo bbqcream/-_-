e1e2bc70b83fa9c7df71ac82491e9a4ac5054235
김성한 <imground11@gmail.com>
2025-01-18 22:59:40 +0900

    Update stack.py

--- stack.py
+++ stack.py
@@ -34,3 +34,30 @@ def peek(): # 최댓값 찾기
         return array[top] # 젤 위에꺼 반환
     else:
         pass
+    
+## 괄호 검사 문제 ##
+
+# 조건1 : 왼쪽 괄호의 개수와 오른쪽 괄호의 개수가 같아야 함. ex) '([{}] 안됨'
+# 조건2 : 같은 타입의 괄호에서 왼쪽 괄호가 오른쪽 괄호보다 먼저 나와야 함 ex) '][ 안됨'
+# 조건3 : 서로 다른 타입의 괄호 쌍이 서로를 교차하면 안됨 ex) ' [ }, {) 안됨'
+
+def checkBrackets(statement):
+    stack = ArrayStack(100) # 용량이 100인 스택 객체 생성
+    openParentheses = ["{", '[', '(']
+    closeParentheses= ['}', ']', ')']
+    
+    for ch in statement : # 입력 값을 하나씩 검사
+        if ch in openParentheses: # 여는 괄호 중 하나가 같다면
+            stack.push(ch) # 스택에 삽입
+            
+        elif ch in closeParentheses: # 닫는 괄호 중 하나가 같다면
+            if stack.isEmpty(): # 비었으면
+                return False # 조건 2 위반 (비었는데 닫는 괄호가 나왔다는 거는 오른쪽 괄호가 왼쪽 괄호보다 먼저 나왔잖아)
+            else:
+                left = stack.pop()
+                if (ch == openParentheses[0] and left !=closeParentheses[0]) or (ch == openParentheses[1] and left!=closeParentheses[1]) or (ch==openParentheses[2] and left !=closeParentheses[2]):
+                    return False # 조건 3 위반
+                
+    return stack.isEmpty() # False 반환되면 조건 1 위반 (뭐 하나는 여는 괄호만 들어갔다는 얘기)
+                
+    
